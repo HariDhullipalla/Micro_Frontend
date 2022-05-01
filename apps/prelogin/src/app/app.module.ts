981f0707f6e267700cc92b7ebf9be24eefdba421
Anjaneyulu18 <durganjaneyulu1818@gmail.com>
2022-05-01 11:47:25 +0530

    First Commit

--- apps/prelogin/src/app/app.module.ts
+++ apps/prelogin/src/app/app.module.ts
@@ -4,9 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
+import { Route, RouterModule } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+const routes: Route[] = [
+  {
+    path: 'post-login-panel', 
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        remoteName: 'post-login',
+        exposedModule: './Module',
+      }).then((m:any) => m.PostLoginModule),
+  },
+];
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes, { initialNavigation: 'enabled' })],
   providers: [],
   bootstrap: [AppComponent],
 })
